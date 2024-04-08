52c1e1bd87930e3caea5854b73fb19bd11846310
CarlosRa10 <carloseduardoramirez10@gmail.com>
2024-04-08 11:52:33 -0400

    godotmania

--- Scenes/Characters/ninjafrog.gd
+++ Scenes/Characters/ninjafrog.gd
@@ -6,12 +6,15 @@ const JUMP_VELOCITY = -450.0
 var allow_animation:bool = false
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var direction 
 var leaved_floor: bool = false
 var had_jump: bool = false
 var max_jumps: int = 2
 var count_jumps: int = 0
 var double_jump:bool = false
-var ray_cast_dimension = 10.5
+var ray_cast_dimension = 11
+var stuck_on_wall:bool = false
+
 
 func _ready():
 	$animaciones.play("Appear")
@@ -38,56 +41,69 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	#print(velocity)
-	move_and_slide()
-	decide_animation()
-	print($raycast_wall_jump.get_collider())
+	print($raycast_wall_jump.get_collider())# y el get_collider te consigue es el objeto colisionador = te dice que tocas
 	if $raycast_wall_jump.get_collider():
 		if $raycast_wall_jump.get_collider().is_in_group("wall_jump"):
-			print("Tocando la zona amarilla")
+			#print("Tocando la zona amarilla")
+			if velocity.y > 0:
+				count_jumps = 0
+				velocity.y = 0
+				stuck_on_wall = true
+		else:
+			stuck_on_wall = false
+	else: 
+		stuck_on_wall = false
+	move_and_slide()
+	decide_animation()
+	#el raycast_wall_jump.is_colliding es para ver si el raycast esta tocando algo
+
 func decide_animation():
+	
+	if direction < 0:
+		$animaciones.flip_h = true
+		$raycast_wall_jump.target_position.x = -ray_cast_dimension
+	elif direction > 0:
+		$animaciones.flip_h = false
+		$raycast_wall_jump.target_position.x = ray_cast_dimension
 	if double_jump:
 		double_jump = false
 		allow_animation = false
-		if velocity.x < 0:
-			$animaciones.flip_h = true
-		elif velocity.x > 0:
-			$animaciones.flip_h = false
 		
 		$animaciones.play("Double_jump")
 	if not allow_animation: return #No va a hacer caso de las animaciones a no ser que sea ese bool sea true
 	#Eje de las X
-	if velocity.x == 0:
-		$animaciones.play("Idle")
-		#Idle
-	elif velocity.x < 0:
-		#Izquierda - Left
-		$raycast_wall_jump.target_position.x = -ray_cast_dimension
-		$animaciones.flip_h = true
-		$animaciones.play("Run")
-	elif velocity.x > 0:
-		#Derecha - Right
-		$raycast_wall_jump.target_position.x = ray_cast_dimension
-		$animaciones.flip_h = false
-		$animaciones.play("Run")
-	#Eje de las y
-	if velocity.y > 0:
-		#Cayendo
-		$animaciones.play("Jump_down")
-	elif velocity.y < 0:
-		#Saltando
-		$animaciones.play("Jump_up")
+	if stuck_on_wall:
+		$animaciones.play("Wall_jump")
+	else:
+		if velocity.x == 0:
+			$animaciones.play("Idle")
+			#Idle
+		elif velocity.x < 0:
+			#Izquierda - Left
+			$animaciones.play("Run")
+		elif velocity.x > 0:
+			#Derecha - Right
+			$animaciones.flip_h = false
+			$animaciones.play("Run")
+		#Eje de las y
+		if velocity.y > 0:
+			#Cayendo
+			$animaciones.play("Jump_down")
+		elif velocity.y < 0:
+			#Saltando
+			$animaciones.play("Jump_up")
 
 func right_to_jump():
 	if had_jump: 
 		if count_jumps < max_jumps: return true
 		else: return false
-	if is_on_floor(): 
+	if is_on_floor() || stuck_on_wall: 
 		had_jump = true
 		return true
 	elif not $Coyote_timer.is_stopped():
